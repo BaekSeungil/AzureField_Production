b0f4d8c5b503c45ec5a87fec4535f7cb12b4cbf2
Hannozero <hanno31@naver.com>
2024-02-28 19:30:48 +0900

    Camera Distance Object Activate writing......

---
+++ Assets/PersonalWorks/Nid100/CameraDistanceObjectActivate.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  카메라와 오브젝트의 거리에 따라서 오브젝트를 활성화/비활성화하는 스크립트입니다.
+/// </summary>
+public class CameraDistanceObjectActivate : MonoBehaviour
+{
+
+    private Transform playerTransform;
+
+
+
+}
