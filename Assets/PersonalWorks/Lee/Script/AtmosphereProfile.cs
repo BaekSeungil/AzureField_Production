0b930ea7d8d626551ed8e942d0b47e53527f7bf8
leenum <144374703+Usenaver@users.noreply.github.com>
2024-03-23 21:18:56 +0900

    1

--- Assets/PersonalWorks/Lee/Script/AtmosphereProfile.cs
+++ Assets/PersonalWorks/Lee/Script/AtmosphereProfile.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
+
+enum Weather
+{
+    Sun = 0,
+    Cloud = 1,
+    Rain = 2
+};
 public class AtmosphereProfile : ScriptableObject
 {
+    [Header("시간설정")]
+    [SerializeField] private float SetTimer;
+
     
     void Start()
     {
