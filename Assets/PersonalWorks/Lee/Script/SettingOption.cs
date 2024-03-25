9f3e2046b153291fc2cdfe38a947bbd4cce741f7
leenum <144374703+Usenaver@users.noreply.github.com>
2024-03-25 22:00:37 +0900

    중간

---
+++ Assets/PersonalWorks/Lee/Script/SettingOption.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+
+public class SettingOption : MonoBehaviour
+{
+   
+   [Header("그래픽 세팅")]
+   [SerializeField]
+   List<RenderPipelineAsset> RenderPipeLine;
+
+   public void SetPipeLine(int value)
+   {
+    
+   }
+
+}
