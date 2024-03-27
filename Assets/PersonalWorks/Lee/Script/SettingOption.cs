6805a99ac4138292bd0e61c7b06497a053d4542e
leenum <144374703+Usenaver@users.noreply.github.com>
2024-03-27 14:11:44 +0900

    환결설정조정

--- Assets/PersonalWorks/Lee/Script/SettingOption.cs
+++ Assets/PersonalWorks/Lee/Script/SettingOption.cs
@@ -4,77 +4,91 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Rendering;
+using Unity.Entities;
 
 public class SettingOption : MonoBehaviour
 {
-   FMOD.Studio.EventInstance VolumeEvent;
-   FMOD.Studio.Bus Music;
-   FMOD.Studio.Bus SFX;
-   FMOD.Studio.Bus Master;
 
-   
-   [Header("환경설정")]
-    [SerializeField] float MusicVolme;
+   [SerializeField] GameObject Setting;
+   [SerializeField] GameObject SoundSetting;
+   [SerializeField] GameObject GrapicSetting;
+   [SerializeField] GameObject MoveSetting;
 
-    [SerializeField] float SFXVolme;
-    [SerializeField] float MasterVolme;
-   [SerializeField] List<RenderPipelineAsset> RenderPipeLine;
-   [SerializeField] TMP_Dropdown Dropdown;
-
+   private bool GameIsPaused = false;
 
    private void Awake() 
-   {
-      Music = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
-      SFX = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX");
-      Master = FMODUnity.RuntimeManager.GetBus("bus:/Master");
-      VolumeEvent = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/VolumeEvent");
-   }
-
-   private void Update() 
-   {
-      Music.setVolume(MusicVolme);
-      SFX.setVolume(SFXVolme);
-      Master.setVolume(MasterVolme);
-   }
-
-   //그래픽 파이프라인
-   public void SetPipeLine(int value)
-   {
-        QualitySettings.SetQualityLevel(value);
-        QualitySettings.renderPipeline = RenderPipeLine[value];
-   }
-
-   public void MasterVolumeLevel(float newMasterVolume)
-   {
-      MasterVolme = newMasterVolume;
-   }
-   public void MusicVolumeLevel(float newMusicVolume)
-   {
-      MasterVolme =  newMusicVolume;
-   }
-
-   public void SFXVolumeLevel(float newSFXVolume)
-   {
-      MasterVolme =  newSFXVolume;
-      FMOD.Studio.PLAYBACK_STATE PbState;
-      VolumeEvent.getPlaybackState(out PbState);
-      if(PbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
-      {
-         VolumeEvent.start();
-      }
-
-   }
-
-   public void InButton()
    {
       
    }
 
-   public void ExitWindow()
+   private void Update() 
    {
-      Destroy(gameObject);
+      if(Input.GetKeyDown(KeyCode.P))
+      {
+         if(GameIsPaused)
+         {
+            Resume();
+         }
+         else
+         {
+            Pasue();
+         }
+      }
    }
 
+   public void Resume()
+   {
+      Setting.SetActive(false);
+      Time.timeScale = 1f;
+      GameIsPaused = false;
+   }
 
+   public void Pasue()
+   {
+      Setting.SetActive(true);
+      Time.timeScale = 0f;
+      GameIsPaused  = true;
+   }
 
+   public void SetSoundprefab()
+   {
+      SoundSetting.SetActive(true);
+      Setting.SetActive(false);
+
+   }
+
+   public void ExitSoundprefab()
+   {
+      SoundSetting.SetActive(false);
+      Setting.SetActive(true);
+   }
+
+   public void SetGrapicprefab()
+   {
+      GrapicSetting.SetActive(true);
+      Setting.SetActive(false);
+   }
+
+   public void ExitGrapicprefab()
+   {
+      GrapicSetting.SetActive(false);
+      Setting.SetActive(true);
+   }
+
+   public void SetMoveprefab()
+   {
+      MoveSetting.SetActive(true);
+      Setting.SetActive(false);
+   }
+
+   public void ExitMoveprefab()
+   {
+      MoveSetting.SetActive(false);
+      Setting.SetActive(true);
+   }
+
+   public void QuitGame()
+   {
+      Application.Quit();
+   }
 }
