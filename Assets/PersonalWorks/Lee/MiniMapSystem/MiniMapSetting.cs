ff6725b756015c84c42d17e2b6468cfab726820d
leenum <144374703+Usenaver@users.noreply.github.com>
2024-04-22 16:07:04 +0900

    미니맵 적용

--- Assets/PersonalWorks/Lee/MiniMapSystem/MiniMapSetting.cs
+++ Assets/PersonalWorks/Lee/MiniMapSystem/MiniMapSetting.cs
@@ -1,3 +1,4 @@
+using Mono.Cecil.Cil;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,34 @@ public class MiniMapSetting : MonoBehaviour
 {
     public Transform targetFollow;
     public bool rotateWidthTheTarget = true;
+    [SerializeField] public GameObject minimap;
 
+    private bool SetMinimap = false;
     private void Update() 
     {
-        
+        if(Keyboard.current[Key.M].wasPressedThisFrame)
+        {
+            if(SetMinimap)
+            {
+                Outmap();
+            }
+            else
+            {
+                Setmap();
+            }
+        }
+    }
+
+
+    public void Setmap()
+    {
+        minimap.SetActive(true);
+        SetMinimap = true;
+    }
+
+    public void Outmap()
+    {
+        minimap.SetActive(false);
+        SetMinimap = false;
     }
 }
