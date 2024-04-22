1d89617fcb35ba2b0a6973ec463982c74a26e364
leenum <144374703+Usenaver@users.noreply.github.com>
2024-04-22 17:18:47 +0900

    미니맵 완성

--- Assets/PersonalWorks/Lee/MiniMapSystem/MiniMapSetting.cs
+++ Assets/PersonalWorks/Lee/MiniMapSystem/MiniMapSetting.cs
@@ -2,6 +2,7 @@ using Mono.Cecil.Cil;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 public class MiniMapSetting : MonoBehaviour
 {
@@ -9,20 +10,42 @@ public class MiniMapSetting : MonoBehaviour
     public bool rotateWidthTheTarget = true;
     [SerializeField] public GameObject minimap;
 
+    [SerializeField] RectTransform scrollViewRectTransform;
+
+    [SerializeField] RectTransform contentRectTransform;
+
+    [SerializeField] float zoomSpeed = 0.1f;
+    [SerializeField] float maxZoom= 10f;
+    [SerializeField] float minZoom = 1f;
+
+    private Vector2 scrollViewDeaultSize;
+    private Vector2 scrollViewDefaultPos;
+
     private bool SetMinimap = false;
-    private void Update() 
+
+    private void Awake() 
+    {
+        scrollViewDeaultSize = scrollViewRectTransform.sizeDelta;
+        scrollViewDefaultPos = scrollViewRectTransform.anchoredPosition;
+    }
+
+    void Update() 
     {
         if(Keyboard.current[Key.M].wasPressedThisFrame)
         {
             if(SetMinimap)
             {
                 Outmap();
+                
             }
             else
             {
                 Setmap();
             }
         }
+
+        // float zoom = Mouse.current.scroll.ReadValue().y;
+        // ZoomMap(zoom);
     }
 
 
@@ -37,4 +60,18 @@ public class MiniMapSetting : MonoBehaviour
         minimap.SetActive(false);
         SetMinimap = false;
     }
+
+    private void ZoomMap(float zoom)
+    {
+        if(zoom == 0)
+        return;
+
+        float currentMapScale = contentRectTransform.localScale.x;
+        float zoomAmount = (zoom > 0 ? zoomSpeed : -zoomSpeed) * currentMapScale;
+        float newScale = currentMapScale + zoomAmount;
+        float clampedScale = Mathf.Clamp(newScale, minZoom, maxZoom);
+        contentRectTransform.localScale = Vector3.one * clampedScale;
+    }
+
+
 }
