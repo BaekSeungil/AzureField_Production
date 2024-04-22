5ba8bee86360ba23c2ee0df44788c217aa815a0f
leenum <144374703+Usenaver@users.noreply.github.com>
2024-04-22 15:29:45 +0900

    미니맵 카메라 적용 및 랜더링 이미지 생성

---
+++ Assets/PersonalWorks/Lee/MiniMapSystem/MiniMapSetting.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class MiniMapSetting : MonoBehaviour
+{
+    public Transform targetFollow;
+    public bool rotateWidthTheTarget = true;
+
+    private void Update() 
+    {
+        
+    }
+}
