414577dffe86c883dda1f5fd2c63a3ae9aa9665b
leenum <144374703+Usenaver@users.noreply.github.com>
2024-04-27 20:29:47 +0900

    중간저장

--- Assets/PersonalWorks/Lee/Script/QickTimeSystem.cs
+++ Assets/PersonalWorks/Lee/Script/QickTimeSystem.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using Mono.Cecil.Cil;
 
 public class QickTimeSystem : MonoBehaviour
 {
+
+    public float fillAmount = 0;
+    public float timeThreshold = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,23 @@ public class QickTimeSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Keyboard.current[Key.A].wasPressedThisFrame)
+        {
+            fillAmount += 0.2f;
+        }
+        timeThreshold += Time.deltaTime;
+
+        if(timeThreshold> 0.1f)
+        {
+
+        }
+        timeThreshold = 0;
+        fillAmount -= 0.02f;
         
+        if(fillAmount < 0)
+        {
+            fillAmount = 0;
+        }
+        GetComponent<Image>().fillAmount = fillAmount;
     }
 }
