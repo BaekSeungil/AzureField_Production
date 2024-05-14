38e79f14dafc9225ac9a6a274e0f86dc6bbbb8f6
leenum <144374703+Usenaver@users.noreply.github.com>
2024-05-14 22:24:08 +0900

    중간저장

--- Assets/PersonalWorks/Lee/Script/QickTimeEvent/JumpObject.cs
+++ Assets/PersonalWorks/Lee/Script/QickTimeEvent/JumpObject.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 
 public class JumpObject : MonoBehaviour
 {
-   [SerializeField, LabelText("점프 발판 높이")]public float jumpForce = 10f;
-
+    [SerializeField, LabelText("점프 발판 높이")]public float jumpForce = 10f;
    private void OnTriggerEnter(Collider other) 
    {
-        if (other.gameObject.layer ==6)
+        if (other.gameObject.layer == 6)
         {
-            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
-            if(rb != null)
-            {
-                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            }
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            Debug.Log("플레이어 체크");
+        
         }
    }
 }
