a95bbf85c253b22ca06139c5cced083f18fde9e8
BadToast <ship2042@gmail.com>
2024-05-23 12:42:23 +0900

    minor changes

--- Assets/Scripts/Misc/IntroWarpPlayer.cs
+++ Assets/Scripts/Misc/IntroWarpPlayer.cs
@@ -18,7 +18,6 @@ public class IntroWarpPlayer : MonoBehaviour
     public void EnablePlayer()
     {
         PlayerCore.Instance.EnableControlls();
+        if (MiniMapSetting.IsInstanceValid) MiniMapSetting.Instance.Setmap();
     }
-
-
 }
