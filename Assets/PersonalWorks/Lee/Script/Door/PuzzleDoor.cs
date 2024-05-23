c2c9ea6de57e5fc8aef900e595c19a359c9a8fb4
leenum <144374703+Usenaver@users.noreply.github.com>
2024-05-24 07:13:50 +0900

    11

---
+++ Assets/PersonalWorks/Lee/Script/Door/PuzzleDoor.cs
@@ -0,0 +1,21 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleDoor : MonoBehaviour
+{
+    
+    [SerializeField,LabelText("열쇠의 갯수")] public int KeyCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
