8a398d658bb548c2f72646205bbefcf1d1ec413b
leenum <144374703+Usenaver@users.noreply.github.com>
2024-05-25 03:17:15 +0900

    수정

--- Assets/PersonalWorks/Lee/Script/Door/PuzzleTrigger.cs
+++ Assets/PersonalWorks/Lee/Script/Door/PuzzleTrigger.cs
@@ -6,23 +6,28 @@ using UnityEngine;
 public class PuzzleTrigger : MonoBehaviour
 {
     [SerializeField,LabelText("내려가는 위치값")] public float UnderLocal; 
-    Rigidbody rigidbody;
+    [SerializeField,LabelText("내려가는 속도")] public float speed;
+    private Rigidbody rigid;
 
     PuzzleDoor puzzleDoor;
     // Start is called before the first frame update
    private void Awake() 
    {
-        puzzleDoor = FindObjectOfType<PuzzleDoor>();
+        rigid = GetComponent<Rigidbody>();
    }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
+        puzzleDoor = FindObjectOfType<PuzzleDoor>();
         puzzleDoor.OpenDoorCount += 1;
         
+        rigid.AddForce(Vector3.up * UnderLocal,ForceMode.Impulse);
     }
 
     private void OnTriggerExit(Collider other) 
     {
+        puzzleDoor = FindObjectOfType<PuzzleDoor>();
         puzzleDoor.OpenDoorCount -= 1;
+        rigid.AddForce(Vector3.up * UnderLocal,ForceMode.Impulse);
     }
 }
