5807e848c8f4eb308f28c8aa4fe61ebd77a2ce1c
leenum <144374703+Usenaver@users.noreply.github.com>
2024-05-31 22:27:04 +0900

    물고기 스폰, 물고기 움직임 구현

--- Assets/PersonalWorks/Lee/Script/FishAi/SpawnFish.cs
+++ Assets/PersonalWorks/Lee/Script/FishAi/SpawnFish.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 
 public class SpawnFish : MonoBehaviour
 {
-    [SerializeField,LabelText("물고기 오브젝트 배치")] public GameObject[] gameObjects;
+    [SerializeField,LabelText("물고기 오브젝트 배치")] public GameObject[] FishgameObjects;
+    [SerializeField, LabelText("스폰 간격(초)")] public float spawnInterval = 2f;
+
+    private float timeSinceLastSpawn;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,38 @@ public class SpawnFish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeSinceLastSpawn += Time.deltaTime;
+        if(timeSinceLastSpawn >= spawnInterval)
+        {
+            SpawnFishgameObject();
+            timeSinceLastSpawn = 0f;
+        }
     }
+
+    public void SpawnFishgameObject()
+    {
+        foreach (var fish in FishgameObjects)
+        {
+            if(!fish.activeInHierarchy)
+            {
+                fish.SetActive(true);
+                break;
+            }
+        }
+       
+    }
+
+
+
+    // IEnumerator SpawnFishObject()
+    // {
+    //     while(true)
+    //     {
+    //         int randomIndex  = Random.Range(0, FishgameObjects.Length);
+    //         GameObject fishPrefab = FishgameObjects[randomIndex];
+
+    //         Instantiate(fishPrefab,transform.position,Quaternion.identity);
+    //         yield return new WaitForSeconds(spawnInterval);
+    //     }
+    // }
 }
