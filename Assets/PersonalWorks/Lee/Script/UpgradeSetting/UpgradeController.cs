6570255c2db4ccfefdddb539e99a51e29cb6a8b5
leenum <144374703+Usenaver@users.noreply.github.com>
2024-06-09 15:52:01 +0900

    중간 세이브

--- Assets/PersonalWorks/Lee/Script/UpgradeSetting/UpgradeController.cs
+++ Assets/PersonalWorks/Lee/Script/UpgradeSetting/UpgradeController.cs
@@ -1,10 +1,23 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BoatUpgradeType
+{
+
+};
+
 public class UpgradeController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("업그레이드 설정값")]
+
+    [SerializeField,LabelText("조각배 수직점프력 증가")] public float PlusJump;
+    [SerializeField,LabelText("보트 도약 수직가속증가")] float PlusBoatJump;
+    [SerializeField,LabelText("보트 부스터 지속시간 증가")] public float PlusBoatKeep;
+
+    [SerializeField,LabelText("보트 업그레이드 창")] public GameObject BoatWindow;
+    [SerializeField,LabelText("플레이어 업그레이드 창")] public GameObject PlayerWindow;
     void Start()
     {
         
@@ -14,5 +27,21 @@ public class UpgradeController : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+
+
+
+    public void Inupgrade()
+    {
+        BoatWindow.SetActive(true);
+        PlayerWindow.SetActive(true);
+    }
+
+    public void Outupgrade()
+    {
+        BoatWindow.SetActive(false);
+        PlayerWindow.SetActive(false);
     }
 }
