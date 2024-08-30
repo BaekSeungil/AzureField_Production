5ed832e876dd719978a1aaf7e150d91e2db80a37
leenum <144374703+Usenaver@users.noreply.github.com>
2024-08-31 08:46:37 +0900

    중간2

--- Assets/PersonalWorks/Lee/Script/CallEther.cs
+++ Assets/PersonalWorks/Lee/Script/CallEther.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,32 +8,51 @@ public class CallEther : MonoBehaviour
     MainPlayerInputActions inputActions;
     EtherSystem etherSystem;
 
+    [SerializeField,LabelText("파도 스폰 위치")] public GameObject Spawn;
 
+    private Collider spawnCollider;
 
     void Start()
     {
         etherSystem.GetComponent<EtherSystem>();
         inputActions = new MainPlayerInputActions();
         inputActions.Player.Interact.Enable();
+
+        if(Spawn != null)
+        {
+            spawnCollider = Spawn.GetComponent<Collider>();
+            if(spawnCollider == null)
+            {
+                Debug.Log("스폰에 콜라이더가 없습니다.");
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (inputActions.Player.Interact.triggered)
+ 
+    }
+
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.layer == 4)
         {
-            SendboolTrigger();
+            etherSystem.CalledWave = true;
+        }
+        else
+        {
+            etherSystem.CalledWave = false;
         }
     }
 
-    private void SendboolTrigger()
+
+    //스폰 콜라이더 범위내 있는지 확인
+    private void SpawnRadius()
     {
-        etherSystem.CalledWave = true;
+    
+
     }
 
-    public void CreatedBoxObject()
-    {
-        
-    }
 }
