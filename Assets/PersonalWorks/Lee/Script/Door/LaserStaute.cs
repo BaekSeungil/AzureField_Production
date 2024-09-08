b4568eb62591eafb9c7d751ce6364852cc752fce
leenum <144374703+Usenaver@users.noreply.github.com>
2024-09-09 00:32:41 +0900

    석상 추가

--- Assets/PersonalWorks/Lee/Script/Door/LaserStaute.cs
+++ Assets/PersonalWorks/Lee/Script/Door/LaserStaute.cs
@@ -7,18 +7,26 @@ public class LaserStaute : MonoBehaviour
 {   
     [SerializeField,LabelText("레이저 시작지점")] public Transform startTrans; // 레이저 시작 위치
     [SerializeField,LabelText("오브젝트 지정")] public PuzzleDoor puzzleDoor; // 퍼즐 도어 오브젝트
+
+    [SerializeField,LabelText("석상 오브젝트")] public GameObject StatueObj; // 석상 오브젝트 지정
+
+    [SerializeField,LabelText("레이저 거리")]    public float maxLaserDistance = 20f; // 레이저 최대 거리
     private Vector3 direction; // 레이저 방향
-    LineRenderer laser; // 레이저 시각화를 위한 LineRenderer
-    GameObject tempReflector; // 임시 반사 오브젝트
-    public float maxLaserDistance = 100f; // 레이저 최대 거리
+    private LineRenderer laser; // 레이저 시각화를 위한 LineRenderer
+
+    private Vector3 StartLaserPos; //레이저 초기 좌표값
+    private Vector3 StartStatuePos; // 석상 초기 좌표값
+  
 
     // Start is called before the first frame update
     void Start()
     {
+        StartLaserPos = startTrans.position;
+        StartStatuePos = StatueObj.transform.position;
+
         laser = gameObject.GetComponent<LineRenderer>();
-        direction = startTrans.forward; // 레이저는 시작점의 forward 방향으로 발사됨
+        direction = startTrans.forward; // 레이저는 시작점
         puzzleDoor = FindObjectOfType<PuzzleDoor>(); // 퍼즐 도어 찾기
-        laser.SetPosition(0, startTrans.position);
     }
 
     // Update is called once per frame
@@ -30,14 +38,15 @@ public class LaserStaute : MonoBehaviour
     // 레이저 발사 함수
     void FireLaser()
     {
-        //laser.SetPosition(0, startTrans.position); // 레이저 시작 위치 설정
+        direction = startTrans.forward;
+        laser.SetPosition(0,startTrans.position);
+        laser.transform.rotation = startTrans.rotation; // 레이저 시작 위치 설정
+
         Ray ray = new Ray(startTrans.position, direction); // 레이저를 발사할 Ray 설정
         RaycastHit hit; // 충돌 정보 저장
-
         if (Physics.Raycast(startTrans.position,direction, out hit, maxLaserDistance))
         {
             laser.SetPosition(1, hit.point); // 레이저가 충돌한 지점까지 시각화
-           
         }
         else
         {
@@ -54,6 +63,31 @@ public class LaserStaute : MonoBehaviour
 
     private void OnCollisionStay(Collision other) 
     {
-        
+        if(other.gameObject.layer == 4)
+        {
+
+        }
     }
+
+     private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red; // 레이저 색상
+
+        Vector3 laserStartPos = startTrans.position; // 레이저 시작 지점
+        Vector3 laserEndPos = laserStartPos + startTrans.forward * maxLaserDistance; // 최대 레이저 거리 계산
+
+        RaycastHit hit;
+        if (Physics.Raycast(laserStartPos, startTrans.forward, out hit, maxLaserDistance))
+        {
+            // 레이저가 충돌하면 충돌 지점까지 기즈모로 선 그리기
+            Gizmos.DrawLine(laserStartPos, hit.point);
+            Gizmos.DrawSphere(hit.point, 0.1f); // 충돌 지점에 작은 구 그리기
+        }
+        else
+        {
+            // 충돌하지 않으면 최대 거리까지 선 그리기
+            Gizmos.DrawLine(laserStartPos, laserEndPos);
+        }
+    }
+
 }
