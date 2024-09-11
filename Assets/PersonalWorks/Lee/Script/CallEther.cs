23b2ced3eebdb456fcbd3344ad068744f8b12d92
leenum <144374703+Usenaver@users.noreply.github.com>
2024-09-11 13:05:04 +0900

    11

--- Assets/PersonalWorks/Lee/Script/CallEther.cs
+++ Assets/PersonalWorks/Lee/Script/CallEther.cs
@@ -11,6 +11,8 @@ public class CallEther : MonoBehaviour
 
     [SerializeField,LabelText("파도 스폰 위치")] public GameObject Spawn;
     [SerializeField,LabelText("에테르프리펩")] public GameObject EtherWave;
+    [SerializeField, LabelText("감지 반경")] private float detectionRadius = 5f;
+    [SerializeField,LabelText("레이어 마스크 설정")] public LayerMask layerMask;
     private Collider spawnCollider;
     public bool OnSpawn = false;
     public bool IsCreat = false;
@@ -20,15 +22,6 @@ public class CallEther : MonoBehaviour
         etherSystem = FindObjectOfType<EtherSystem>();
         inputActions = new MainPlayerInputActions();
         inputActions.Player.Ether.Enable();
-
-        if(Spawn != null)
-        {
-            spawnCollider = Spawn.GetComponent<Collider>();
-            if(spawnCollider == null)
-            {
-                Debug.Log("스폰에 콜라이더가 없습니다.");
-            }
-        }
     }
 
     // Update is called once per frame
@@ -36,55 +29,34 @@ public class CallEther : MonoBehaviour
     {
         if(inputActions.Player.Ether.WasPressedThisFrame())
         {
-            if (!IsCreat)
+            Collider[] hitColliders = Physics.OverlapSphere(Spawn.transform.position, detectionRadius, layerMask);
+
+            // 감지된 오브젝트가 있다면 파도를 생성
+            if (hitColliders.Length > 0 && !IsCreat)
             {
                 Instantiate(EtherWave, Spawn.transform.position, Quaternion.identity);
                 IsCreat = true;
                 EtherCount = 1;  // 상태를 1로 설정하여 생성된 상태로 표시합니다.
-                Debug.Log("파도 생성");
+                Debug.Log("파도 생성 (범위 내에 객체 감지)");
             }
             // 파도가 이미 생성된 상태에서 다시 키를 누르면 파도를 이동시킵니다.
             else if (IsCreat && EtherCount == 1)
             {
                 EtherCount = 2;  // 상태를 2로 설정하여 이동 신호를 보냅니다.
                 Debug.Log("파도 이동 신호 전송");
-            }
-        }
-    }
-
-
-    private void OnCollisionEnter(Collision other) 
-    {
-        if(other.gameObject.layer == 3 && other.gameObject.layer == 4)
-        {
-            if(OnSpawn)
-            {
-                if(SpawnRadius(other))
+                if(EtherCount == 2)
                 {
-                    
-                    etherSystem.CalledWave = true;
-
+                    EtherCount = 0;
                 }
             }
         }
     }
 
-
-    //스폰 콜라이더 범위내 있는지 확인
-    private bool SpawnRadius(Collision collision)
+    private void OnDrawGizmosSelected()
     {
-        if(spawnCollider == null)
-            return false;
-        
-        foreach(ContactPoint contact in collision.contacts)
-        {
-            if(spawnCollider.bounds.Contains(contact.point))
-            {
-                return true;
-            }
-        }
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(Spawn.transform.position, detectionRadius);  // 감지 반경을 시각화
+    }   
 
-        return false;
-    }
 
 }
