daed2a068b80001c3258026736a136fabc29a903
leenum <144374703+Usenaver@users.noreply.github.com>
2024-09-29 16:35:16 +0900

    식생 3가지 마무리 아이템 추가 스크립트 수정

--- Assets/Scripts/Object/SeaFlower.cs
+++ Assets/Scripts/Object/SeaFlower.cs
@@ -2,13 +2,19 @@ using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using FMODUnity;
 
 
 public class SeaFlower : Interactable_Base
 {
     [SerializeField, LabelText("아이템 데이터")] private ItemData GetItem;
     [SerializeField, LabelText("아이템 개수")] private int ItemQuantity = 1;
-    [SerializeField, LabelText("사라질 오브젝트")] private GameObject DestoryObj;
+    [SerializeField, LabelText("사라질 오브젝트")] private GameObject[] DestoryObj;
+    [SerializeField, LabelText("사라질 오브젝트 개수")] private int DestoryObjQuantity;
+
+    [SerializeField] private EventReference sound_CarrotPicked;
+    [SerializeField] private EventReference sound_CarrotGone;
     private Animator animator;
     private void Awake()
     {
@@ -23,14 +29,18 @@ public class SeaFlower : Interactable_Base
 
     public override void Interact()
     {
+        Debug.Log("Interact 호출됨");
         StartCoroutine(PickUpFlower());
     }
 
     private IEnumerator PickUpFlower()
     {
-        animator.SetBool("PickUp",true);
+        Debug.Log("PickUpFlower 시작");
+        animator.SetTrigger("Pickup");
         PlayerInventoryContainer.Instance.AddItem(GetItem, ItemQuantity);
-        yield return new WaitForSeconds(5.0f);
-        Destroy(DestoryObj);
+        yield return new WaitForSeconds(1.0f);
+        Debug.Log("오브젝트 파괴 시도");
+        Destroy(DestoryObj[DestoryObjQuantity]);
+        yield return new WaitForSeconds(1.2f);
     }
 }
