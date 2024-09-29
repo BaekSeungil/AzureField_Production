75a01b3aeee224da0b9e3972e850e141d8a9a1f4
leenum <144374703+Usenaver@users.noreply.github.com>
2024-09-29 15:42:49 +0900

    아이템 추가 기능 추가

--- Assets/Scripts/Object/SeaFlower.cs
+++ Assets/Scripts/Object/SeaFlower.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 
 public class SeaFlower : Interactable_Base
 {
-    [SerializeField, LabelText("아이템 데이터")] private ItemData carrotItem;
+    [SerializeField, LabelText("아이템 데이터")] private ItemData GetItem;
     [SerializeField, LabelText("아이템 개수")] private int ItemQuantity = 1;
+    [SerializeField, LabelText("사라질 오브젝트")] private GameObject DestoryObj;
     private Animator animator;
     private void Awake()
     {
@@ -20,8 +21,16 @@ public class SeaFlower : Interactable_Base
         
     }
 
-    private void PickUpFlower()
+    public override void Interact()
     {
-        
+        StartCoroutine(PickUpFlower());
+    }
+
+    private IEnumerator PickUpFlower()
+    {
+        animator.SetBool("PickUp",true);
+        PlayerInventoryContainer.Instance.AddItem(GetItem, ItemQuantity);
+        yield return new WaitForSeconds(5.0f);
+        Destroy(DestoryObj);
     }
 }
