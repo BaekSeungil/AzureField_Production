37171714a29e51f622bbfec337919d2a10e99fd5
leenum <144374703+Usenaver@users.noreply.github.com>
2024-10-05 08:12:37 +0900

    중간저장

--- Assets/PersonalWorks/Lee/Script/UpgradeSetting/PlayerUpgrde.cs
+++ Assets/PersonalWorks/Lee/Script/UpgradeSetting/PlayerUpgrde.cs
@@ -1,9 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Localization;
+using Sirenix.OdinInspector;
+
+public struct PlayerUpLimt
+{
+    public int PlayercurrentCount;
+    public int PlayermaxCount;
+
+    public bool PlayerCanUpgrade() => PlayercurrentCount < PlayermaxCount;
+}
+
+public enum PlayerUpgedeType
+{
+    PlayerJump,
+    PlayerMoveSpeed,
+    WaveSpawnTime
+
+};
 
 public class PlayerUpgrde : MonoBehaviour
 {
+    [Header("업그레이 설정 값")]
+    [SerializeField,LabelText("점프력 증가값")] float PlusJumpPower;
+    [SerializeField,LabelText("이동속도 증가값")] public float PlusMoveSpeed;
+    [SerializeField,LabelText("에테르 시간 증가값")] public float EtherTimeUp;
+
+    [Header("업그레이드 창 설정")]
+    [SerializeField,LabelText("플레이어 업그레이드 창")] public GameObject Upwindow;
     // Start is called before the first frame update
     void Start()
     {
