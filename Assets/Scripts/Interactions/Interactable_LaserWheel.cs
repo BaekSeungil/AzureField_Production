118acbd834af223a0cc9b85688be86dc05f2e13b
BadToast <ship2042@gmail.com>
2024-10-16 18:33:31 +0900

    .

---
+++ Assets/Scripts/Interactions/Interactable_LaserWheel.cs
@@ -0,0 +1,11 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_LaserWheel : SerializedMonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 1.0f;
+    [SerializeField] private float laserMaxLength;
+    [SerializeField] private Transform laserStartPoint;
+}
